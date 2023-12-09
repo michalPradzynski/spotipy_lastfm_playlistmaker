8608f7bdd1803acff31a6411e27ea0c03f7f5946
Michał Prądzyński <41488514+michalPradzynski@users.noreply.github.com>
2023-12-09 11:11:03 +0100

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,6 @@
+LASTFM_USERNAME = "username"
+LASTFM_PASSWORD = "password"
+
+SPOTIPY_CLIENT_ID = "client_id"
+SPOTIPY_CLIENT_SECRET = "client_secret"
+SPOTIPY_REDIRECT_ID = "https://localhost:8888/callback"
